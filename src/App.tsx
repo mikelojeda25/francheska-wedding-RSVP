6fb5b87daaf9764bcc534de5e0e1c18cc8b14616
Michael Joseph Ojeda <mikelojeda25@gmail.com>
2026-05-10 11:28:39 +0800

    fix unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import RSVPSection from './components/RSVPSection'
-import './App.css'
 
-function App() {
+export default function App() {
   return <RSVPSection />
 }
-
-export default App
